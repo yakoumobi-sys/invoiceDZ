6f15cbee6d89a6709b1b715cb3f51b94ad3143fa
yakoumobi-sys <yakoumobi@gmail.com>
2026-06-24 00:03:50 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
